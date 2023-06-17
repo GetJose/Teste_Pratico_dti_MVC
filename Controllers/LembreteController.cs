363e112166344dd449477c68a22e44d7eea03473
Getúlio Santos <98064959+GetJose@users.noreply.github.com>
2023-06-17 08:07:42 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/LembreteController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Teste_Pratico_dti_MVC.Controllers
+{
+    public class LembreteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CriarLembrete()
+        {
+            return View();
+        }
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+    }
+}
---
+++ Data/BancoContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Teste_Pratico_dti_MVC.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<DbContext> options): base(options) 
+        { }   
+    }
+}
