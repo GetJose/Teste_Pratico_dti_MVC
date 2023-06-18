0c155663a97079da38838afe34958448d533e13d
Getúlio Santos <98064959+GetJose@users.noreply.github.com>
2023-06-18 16:49:30 -0300

    ultimo

--- Models/LembreteModel.cs
+++ Models/LembreteModel.cs
@@ -3,40 +3,11 @@
 	public class LembreteModel
 	{
 		//atributos do lembrete
-		public int Id { get; set; }
-		private string nome;
-		private DateTime data;
-		public static int cont;
+		private int Id { get; set; }
+		private string nome { get; set; }
+		private DateTime data { get; set; }
+		private static int cont;
 
-		public string Nome
-		{
-			get { return nome; }
-			set
-			{
-				if (!string.IsNullOrEmpty(value))
-					nome = value;
-				else
-					throw new ArgumentException("O campo do nome não pode estar vazio.");
-			}
-		}
-
-		public DateTime Data
-		{
-			get { return data; }
-			set
-			{
-				if (TesteDataFutura(value))
-					data = value;
-				else
-					throw new ArgumentException("A data informada já passou.");
-			}
-		}
-
-		// Verifica se a data está no futuro
-		private bool TesteDataFutura(DateTime data)
-		{
-			return data > DateTime.Now;
-		}
 
 		// Construtor padrão
 		public LembreteModel()
@@ -50,7 +21,7 @@
 		{
 			cont = cont + 1;
 			//verifica se os campos estão preenchidos corretamente, e só assim cria o lembrete
-			if (!string.IsNullOrEmpty(nome) && TesteDataFutura(data))
+			if (!string.IsNullOrEmpty(nome)  && data > DateTime.Now)
 			{
 				this.nome = nome;
 				this.data = data;
