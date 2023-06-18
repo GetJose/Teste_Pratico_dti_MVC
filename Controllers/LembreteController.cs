f18c7598bbb60338b96c0c605a34190511a52e8a
Getúlio Santos <98064959+GetJose@users.noreply.github.com>
2023-06-18 13:40:10 -0300

    Alterações finais

--- Controllers/LembreteController.cs
+++ Controllers/LembreteController.cs
@@ -2,74 +2,74 @@
 using Teste_Pratico_dti_MVC.Models;
 
 namespace Teste_Pratico_dti_MVC.Controllers
-{   
-    //como ainda não tem Conexão com o BD esta usando a lista de lembretes estatica para salvar os lembretes
-    public static class LembreteRepository
-    {
-        public static ListaLembreteModel ListaLembretes { get; } = new ListaLembreteModel();
-    }
-    public class LembreteController : Controller
-    {   
-        //Encaminha para a view de criar o lembrete
-        public IActionResult CriarLembrete()
-        {
-            return View();
-        }
+{
+	//como ainda não tem Conexão com o BD esta usando a lista de lembretes estatica para salvar os lembretes
+	public static class LembreteRepository
+	{
+		public static ListaLembreteModel ListaLembretes { get; } = new ListaLembreteModel();
+	}
+	public class LembreteController : Controller
+	{
+		//Encaminha para a view de criar o lembrete
+		public IActionResult CriarLembrete()
+		{
+			return View();
+		}
 
-        [HttpPost]
-        public ActionResult CriarLembrete(LembreteModel lembrete)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    // Verificar se o nome está vazio
-                    if (string.IsNullOrWhiteSpace(lembrete.Nome))
-                    {
-                        ModelState.AddModelError("Nome", "O campo nome é obrigatório. Insira um nome válido.");
-                        return View(lembrete);
-                    }
-                    // Verificar se a data informada é válida
-                    if (lembrete.Data < DateTime.Now)
-                    {
-                        ModelState.AddModelError("Data", "A data informada já passou. Insira uma data futura.");
-                        return View(lembrete);
-                    }
+		[HttpPost]
+		public ActionResult CriarLembrete(LembreteModel lembrete)
+		{
+			try
+			{
+				if (ModelState.IsValid)
+				{
+					// Verificar se o nome está vazio
+					if (string.IsNullOrWhiteSpace(lembrete.Nome))
+					{
+						ModelState.AddModelError("Nome", "O campo nome é obrigatório. Insira um nome válido.");
+						return View(lembrete);
+					}
+					// Verificar se a data informada é válida
+					if (lembrete.Data < DateTime.Now)
+					{
+						ModelState.AddModelError("Data", "A data informada já passou. Insira uma data futura.");
+						return View(lembrete);
+					}
 
-                    // Adicionar o lembrete à lista usando a instância de ListaLembretes compartilhada
-                    LembreteRepository.ListaLembretes.AdicionarLembrete(lembrete);
+					// Adicionar o lembrete à lista usando a instância de ListaLembretes compartilhada
+					LembreteRepository.ListaLembretes.AdicionarLembrete(lembrete);
 
-                    return RedirectToAction("Index"); // Redirecionar para a página inicial após a criação do lembrete
-                }
-                else
-                {
-                    return View(lembrete);
-                }
-            }
-            catch (ArgumentException ex)
-            {
-                ModelState.AddModelError("", ex.Message);
-                return View(lembrete);
-            }
-        }
+					return RedirectToAction("Index"); // Redirecionar para a página inicial após a criação do lembrete
+				}
+				else
+				{
+					return View(lembrete);
+				}
+			}
+			catch (ArgumentException ex)
+			{
+				ModelState.AddModelError("", ex.Message);
+				return View(lembrete);
+			}
+		}
 
-        public IActionResult Index()
-        {   //Criar a lista que aparece na view de acordo com a lista de lembrete 
-            ListaLembreteModel model = LembreteRepository.ListaLembretes;
+		public IActionResult Index()
+		{   //Criar a lista que aparece na view de acordo com a lista de lembrete 
+			ListaLembreteModel model = LembreteRepository.ListaLembretes;
 
-            if (model.ListaLembretes.Any())//se ela não estiver vazia é ordenada para que as datas sejam mostradas em ordem cronologica 
-            {
-                model.ListaLembretes = model.ListaLembretes.OrderBy(d => d.Key).ToDictionary(d => d.Key, d => d.Value);
-            }
+			if (model.ListaLembretes.Any())//se ela não estiver vazia é ordenada para que as datas sejam mostradas em ordem cronologica 
+			{
+				model.ListaLembretes = model.ListaLembretes.OrderBy(d => d.Key).ToDictionary(d => d.Key, d => d.Value);
+			}
 
-            return View(model);
-        }
+			return View(model);
+		}
 
-        public IActionResult Apagar(string nome)
-        {
-             LembreteRepository.ListaLembretes.RemoverLembrete(nome);
-                return RedirectToAction("Index"); 
-        }
+		public IActionResult Apagar(int id)
+		{
+			LembreteRepository.ListaLembretes.RemoverLembrete(id);
+			return RedirectToAction("Index");
+		}
 
-    }
+	}
 }
--- Models/LembreteModel.cs
+++ Models/LembreteModel.cs
@@ -1,57 +1,65 @@
 ﻿namespace Teste_Pratico_dti_MVC.Models
 {
-    public class LembreteModel
-    {
-            //atributos do lembrete
-            private string nome;
-            private DateTime data;
+	public class LembreteModel
+	{
+		//atributos do lembrete
+		public int Id { get; set; }
+		private string nome;
+		private DateTime data;
+		public static int cont;
 
-            public string Nome
-            {
-                get { return nome; }
-                set
-                {
-                    if (!string.IsNullOrEmpty(value))
-                        nome = value;
-                    else
-                        throw new ArgumentException("O campo do nome não pode estar vazio.");
-                }
-            }
+		public string Nome
+		{
+			get { return nome; }
+			set
+			{
+				if (!string.IsNullOrEmpty(value))
+					nome = value;
+				else
+					throw new ArgumentException("O campo do nome não pode estar vazio.");
+			}
+		}
 
-            public DateTime Data
-            {
-                get { return data; }
-                set
-                {
-                    if (TesteDataFutura(value))
-                        data = value;
-                    else
-                        throw new ArgumentException("A data informada já passou.");
-                }
-            }
+		public DateTime Data
+		{
+			get { return data; }
+			set
+			{
+				if (TesteDataFutura(value))
+					data = value;
+				else
+					throw new ArgumentException("A data informada já passou.");
+			}
+		}
 
-            // Verifica se a data está no futuro
-            private bool TesteDataFutura(DateTime data)
-            {
-                return data > DateTime.Now;
-            }
+		// Verifica se a data está no futuro
+		private bool TesteDataFutura(DateTime data)
+		{
+			return data > DateTime.Now;
+		}
 
-            // Construtor padrão
-            public LembreteModel() { }
-            
-            // construtor para os lembretes com os campos nome e data 
-            public LembreteModel(string nome, DateTime data)
-            {   
-                //verifica se os campos estão preenchidos corretamente, e só assim cria o lembrete
-                if (!string.IsNullOrEmpty(nome) && TesteDataFutura(data))
-                {
-                    this.nome = nome;
-                    this.data = data;
-                }
-                else
-                {
-                    throw new ArgumentException("Os campos do lembrete não estão preenchidos corretamente.");
-                }
-            }
-        }
-    }
+		// Construtor padrão
+		public LembreteModel()
+		{
+			cont = cont + 1;
+			this.Id = cont;
+		}
+
+		// construtor para os lembretes com os campos nome e data 
+		public LembreteModel(string nome, DateTime data)
+		{
+			cont = cont + 1;
+			//verifica se os campos estão preenchidos corretamente, e só assim cria o lembrete
+			if (!string.IsNullOrEmpty(nome) && TesteDataFutura(data))
+			{
+				this.nome = nome;
+				this.data = data;
+				this.Id = cont;
+			}
+			else
+			{
+				throw new ArgumentException("Os campos do lembrete não estão preenchidos corretamente.");
+			}
+		}
+	}
+}
